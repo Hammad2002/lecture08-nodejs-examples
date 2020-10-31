bf3b73a63f3c25620af52f653adb4ba9ddefd5c9
Batatia, Hadj <hb149@hw.ac.uk>
2020-11-01 00:44:33 +0400

    updated names

--- app2-module.js
+++ app2-module.js
@@ -1,4 +1,4 @@
 //import the module
-const log = require("./logger")
-    //use the imported function
+const log = require("./app2-logger");
+//use the imported function
 log("This is to say that I am able to use an imported function");
